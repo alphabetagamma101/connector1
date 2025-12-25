2204e26ba76973233df88f20226dbe07ef148850
amfin2024 <amfin2024@gmail.com>
2025-12-24 20:08:12 -0800

    updated

--- src/proprietary/Proprietary.ts
+++ src/proprietary/Proprietary.ts
@@ -12,21 +12,30 @@ export class Proprietary {
     if (url.includes('cnn.com')) {
       return JSON.stringify({
         url: url,
-        steps: [
+        "steps" : [
           {
-            type: 'type_content',
-            locator: 'input[type="search"] || #search-input',
-            locatorIndex: 'first',
-            content: 'PH_CONTENT'
+            "type" : "type_content",
+            "locator" : "div[contenteditable='true']",
+            "content" : "PH_CONTENT"
           },
           {
-            type: 'wait',
-            waitTimeInMillis: '2000'
+            "type" : "wait",
+            "waitTimeInMillis" : "2000"
           },
           {
-            type: 'move_mouse_and_click',
-            locator: 'button[type="submit"] || .search-button',
-            locatorIndex: 'first'
+            "type": "move_mouse_and_click",
+            "locator": "button[id='composer-submit-button']"
+          },
+          {
+            "type" : "wait_till_present",
+            "locatorIndex" : "last",
+            "locator" : "svg path[d='M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm0,192a84,84,0,1,1,84-84A84.09,84.09,0,0,1,128,212Zm40-112v56a12,12,0,0,1-12,12H100a12,12,0,0,1-12-12V100a12,12,0,0,1,12-12h56A12,12,0,0,1,168,100Z']",
+            "waitTimeInMillis" : "5000"
+          },
+          {
+            "type" : "move_mouse_and_click",
+            "locatorIndex" : "last",
+            "locator" : "button[aria-label='Copy']"
           }
         ]
       });

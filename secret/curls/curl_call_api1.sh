c18f4eff148507f30c2c5d128fd99447ee430ed4
amfin2024 <amfin2024@gmail.com>
2025-12-24 20:13:55 -0800

    updated

--- secret/curls/curl_call_api1.sh
+++ secret/curls/curl_call_api1.sh
@@ -7,5 +7,5 @@ curl -X POST http://localhost:3000/api/invoke \
   -H "Content-Type: application/json" \
   -d '{
     "content": "hello",
-    "url": "https://www.cnn.com"
+    "url": "https://chatgpt.com/"
   }'

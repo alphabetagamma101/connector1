1d4288a4893dda227f513812a45ff08cba84703a
amfin2024 <amfin2024@gmail.com>
2025-12-26 12:27:42 -0800

    updated

--- secret/curls/curl_call_api1.sh
+++ secret/curls/curl_call_api1.sh
@@ -1,6 +1,6 @@
 curl -X POST http://localhost:3000/api/invoke \
   -H "Content-Type: application/json" \
   -d '{
-    "content": "hello",
+    "content": "hello, what is the current time in PST",
     "url": "https://chatgpt.com/"
   }'
